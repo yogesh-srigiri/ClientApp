a1fe5156d3960d37167140053f4c1aa9fdb8d491
yogesh-srigiri <55821875+yogesh-srigiri@users.noreply.github.com>
2020-01-20 15:31:11 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,6 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD3Wn1vj38VFYiw-2LySzeSz-UfU7oORWk",
-    authDomain: "client-app-85ae0.firebaseapp.com",
-    databaseURL: "https://client-app-85ae0.firebaseio.com",
-    projectId: "client-app-85ae0",
-    storageBucket: "client-app-85ae0.appspot.com",
-    messagingSenderId: "7180867928",
-    appId: "1:7180867928:web:0c7fbf7cc82f17fde62d2d",
-    measurementId: "G-T5S8YY7SS2"
+ 
   }
 };
